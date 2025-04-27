6ae3f99c4368df8d33a815350176d197abe2af76
Alexis12119 <corporal461@gmail.com>
2025-04-28 07:55:25 +0800

    feat: update

--- script.js
+++ script.js
@@ -7,9 +7,19 @@ const passwords = {
 
 function enterPassword(grade) {
   const input = prompt("Ilagay ang password para sa " + grade.toUpperCase());
+  
+  if (input === null) {
+    // User pressed Cancel, just exit
+    return;
+  }
+
   if (input === passwords[grade]) {
     window.location.href = `lessons/${grade}.html`;
+  } else if (input === "") {
+    alert("Wala ka pang inilalagay na password.");
+    enterPassword(grade);
   } else {
     alert("Maling password. Subukang muli.");
+    enterPassword(grade);
   }
 }
